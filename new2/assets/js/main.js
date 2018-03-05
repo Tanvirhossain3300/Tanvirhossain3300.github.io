288bb6cd60bc0e17c7280957c2bb4a1a0d124c74
tanvirhossain3300 <tanvirhossain3300@github.com>
2018-03-06 01:38:59 +0600

    my 2

---
+++ new2/assets/js/main.js
@@ -0,0 +1,20 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+    
+       $(".testimonial-author-name ").perfectScrollbar();
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
